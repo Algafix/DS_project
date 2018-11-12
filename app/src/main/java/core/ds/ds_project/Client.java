c21679c92d6d03c5b7181486da4bc5f4515c909c
Adria <adria.figuerola.torrell@gmail.com>
2018-11-12 14:26:40 +0100

    Millores d'estil!!

--- app/src/main/java/core/ds/ds_project/Client.java
+++ app/src/main/java/core/ds/ds_project/Client.java
@@ -8,26 +8,32 @@ import java.io.ObjectOutputStream;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Locale;
+
 
 public class Client {
 
+    private static final int secondsHour = 3600;
+    private static final int minutesHour = 60;
+
 
     /**
-     * Convert object duration to string with format HH:mm:ss
+     * Convert object duration to string with format HH:mm:ss.
+     *
+     * @param duration .
+     * @return the duration with format HH:mm:ss
      */
-    public static String formatDuration(Duration duration) {
-        if(duration == null) {
+    public static String formatDuration(final Duration duration) {
+        if (duration == null) {
             return null;
-        }else {
+        } else {
             long seconds = duration.getSeconds();
             long absSeconds = Math.abs(seconds);
-            String positive = String.format(
+            String positive = String.format(Locale.getDefault(),
                     "%02d:%02d:%02d",
-                    absSeconds / 3600,
-                    (absSeconds % 3600) / 60,
-                    absSeconds % 60);
+                    absSeconds / secondsHour,
+                    (absSeconds % secondsHour) / minutesHour,
+                    absSeconds % minutesHour);
             return seconds < 0 ? "-" + positive : positive;
         }
 
@@ -35,11 +41,10 @@ public class Client {
 
     public static String formatDateTime(LocalDateTime time) {
 
-        if(time != null ){
+        if (time != null ) {
             final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
             return time.format(formatter);
-        }
-        else{
+        } else {
             return "                       ";
         }
     }
@@ -80,39 +85,34 @@ public class Client {
      *
      * @param filename the name of the file where the project is going to be loaded from
      */
-    public static Project deserializeProject(String filename){
+    public static Project deserializeProject(final String filename) {
         // Deserialization
-        try
-        {
+        try {
             // Reading the object from a file
             FileInputStream file = new FileInputStream(filename);
             ObjectInputStream in = new ObjectInputStream(file);
 
             // Method for deserialization of object
-            Project deserializeProject = (Project)in.readObject();
+            Project deserializeProject = (Project) in.readObject();
 
             in.close();
             file.close();
 
             System.out.println("Object has been deserialize ");
             return deserializeProject;
-        }
+        } catch(IOException ex) {
 
-        catch(IOException ex)
-        {
             System.out.println("IOException is caught");
             return null;
-        }
+        } catch(ClassNotFoundException ex) {
 
-        catch(ClassNotFoundException ex)
-        {
             System.out.println("ClassNotFoundException is caught");
             return null;
         }
     }
 
 
-    public static void main(String [] args) {
+    public static void main(final String[] args) {
 
         AppClock.getInstance(2000);
 
