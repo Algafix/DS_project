926a53ed1ed77212b7483a485e3b007804bcd6d2
Adria <adria.figuerola.torrell@gmail.com>
2018-11-12 23:19:27 +0100

    MainActivity class with no checkstyle problems

--- app/src/main/java/core/ds/ds_project/MainActivity.java
+++ app/src/main/java/core/ds/ds_project/MainActivity.java
@@ -3,10 +3,15 @@ package core.ds.ds_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Not necessary all ready understandable.
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Not necessary all ready understandable.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
