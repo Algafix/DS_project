f57300e00b2879e935c94146f0e297d62cd361e9
Adria <adria.figuerola.torrell@gmail.com>
2018-11-13 11:05:24 +0100

    Visitor Class with no checkstyle problems

--- app/src/main/java/core/ds/ds_project/Visitor.java
+++ app/src/main/java/core/ds/ds_project/Visitor.java
@@ -1,10 +1,23 @@
 package core.ds.ds_project;
 
+/**
+ * Not necessary all ready understandable.
+ */
 abstract class Visitor {
-
+    /**
+     * Not necessary all ready understandable.
+     * @param project .
+     */
     public abstract void visitProject(Project project);
 
+    /**
+     * Not necessary all ready understandable.
+     * @param task .
+     */
     public abstract void visitTask(Task task);
-
+    /**
+     * Not necessary all ready understandable.
+     * @param interval .
+     */
     public abstract void visitInterval(Interval interval);
 }
