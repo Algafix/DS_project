14b75a1a3128092e6d2687910f9ddfb02be39517
Adria <adria.figuerola.torrell@gmail.com>
2018-11-14 08:04:51 +0100

    ReportSaver with no checkstyle problems

--- app/src/main/java/core/ds/ds_project/ReportSaver.java
+++ app/src/main/java/core/ds/ds_project/ReportSaver.java
@@ -1,8 +1,23 @@
 package core.ds.ds_project;
 
 import java.util.List;
-
+/**
+ * Not necessary understandable.
+ */
 public abstract class ReportSaver {
-    public abstract void save(Report report, List<String> subtitlesList, String filename);
+    /**
+     * to save a report.
+     * @param report .
+     * @param subtitlesList .
+     * @param filename .
+     */
+    public abstract void save(Report report,
+                              List<String> subtitlesList,
+                              String filename);
+    /**
+     * to save a file.
+     * @param filename .
+     * @param sb string builder
+     */
     protected abstract void saveFile(String filename, StringBuilder sb);
 }
